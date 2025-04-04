13cc04f90a9d7c4ed79fb3fc747782e0e1f6d0c3
dev soft <dev.soft687@gmail.com>
2025-04-05 03:29:35 +0530

    changes

--- src/components/UploadedImages.jsx
+++ src/components/UploadedImages.jsx
@@ -25,19 +25,19 @@ const handleDelete = async (id) => {
 return (
     <div>
         <h1 className="text-center my-5 text-2xl font-bold">All Images</h1>
-        <div className="uploaded-images flex flex-wrap gap-5 justify-center">
+        <div className="uploaded-images grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 justify-center">
             {images.map((image) => (
                 <div 
                     key={image._id} 
-                    className="image-card w-72 border border-gray-300 rounded-lg shadow-md overflow-hidden text-center bg-white transform transition-transform duration-300 hover:scale-105 hover:-translate-y-2"
+                    className="image-card w-full border border-gray-300 rounded-lg shadow-md overflow-hidden text-center bg-white transform transition-transform duration-300 hover:scale-105 hover:-translate-y-2"
                 >
                     <img 
                         src={`${baseUrl}/${image.filePath}`} 
                         alt={image.filename} 
-                        className="w-full h-52 object-cover transform transition-transform duration-300 hover:scale-110"
+                        className="w-full h-52 object-contain transform transition-transform duration-300 hover:scale-110"
                     />
                     <div className="p-4">
-                        <h3 className="text-lg font-medium mb-2">{image.filename}</h3>
+                        {/* <h3 className="text-lg font-medium mb-2">{image.filename}</h3> */}
                         <button 
                             className="delete-button px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 transition"
                             onClick={() => handleDelete(image._id)}
